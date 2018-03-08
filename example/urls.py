e4974b724fd8d7eca19d1bd20c2400a56c5a0deb
Pior Bastida <pior@pbastida.net>
2018-03-07 21:38:45 -0500

    Use official slack client + add tests

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,15 @@
-from django.urls import include, path
+import django
 
-urlpatterns = [
-    path('slackin', include('django_slackin_public.urls')),
-]
+
+if django.VERSION >= (2,):
+    from django.urls import include, path
+
+    pattern = path('slackin', include('django_slackin_public.urls'))
+
+else:
+    from django.conf.urls import include, url
+
+    pattern = url(r'^slackin', include('django_slackin_public.urls'))
+
+
+urlpatterns = [pattern]
